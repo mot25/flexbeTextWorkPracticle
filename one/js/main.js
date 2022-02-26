76a08084ab574a8b926d0a1f457e9f08e49ec4c6
mot25 <mot.112511.pog@gmail.com>
2022-02-26 13:21:09 +0300

    test one comlete

--- one/js/main.js
+++ one/js/main.js
@@ -1,22 +1,13 @@
 const modal = document.querySelector(".modal")
-console.log('modal', modal)
 const img = document.querySelector('.image')
-console.log(img)
 const hModal = modal.offsetHeight
+const himg = modal.offsetHeight
 let heightImg = hModal * 80 / 100
 img.style.height = heightImg + 'px'
 
-/**
-900 100
-x   84 
-
- */
-
 const resizeImg = () => {
-    const himg = modal.offsetHeight
-    // console.log('himg', himg)
-
     img.style.height = heightImg + 'px'
 }
 
 window.addEventListener('resize', resizeImg)
+window.addEventListener('orientationchange', resizeImg)
